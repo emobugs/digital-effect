885986ac56f6d9eeaa00cc87d1d5ebb4f1520ffb
emo_bugs <emotupev11@gmail.com>
2026-02-28 12:20:47 +0200

    favicons import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="bg" className={`${inter.variable} ${montserrat.variable}`}>
+			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+			<link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+			<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+			<link rel="manifest" href="/site.webmanifest" />
 			<body>
 				<Cursor />
 				{children}
