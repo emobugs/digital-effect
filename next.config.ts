f8e7b7fbbf683626060b77ac001d9c1ced0af8df
emo_bugs <emotupev11@gmail.com>
2026-04-08 15:30:02 +0300

    hostinger build edits

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    cpus: 1,
+  },
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.parallelism = 1;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
